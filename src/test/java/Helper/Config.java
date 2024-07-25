fd9ae6ce01d0dcf5d1c1945480d925a7a1ce0ab4
brahimbounguicha <44786262+brahimbounguicha@users.noreply.github.com>
2024-07-25 21:45:55 +0200

    some change

--- src/test/java/Helper/Config.java
+++ src/test/java/Helper/Config.java
@@ -6,6 +6,10 @@ import org.openqa.selenium.interactions.Actions;
 public class Config {
 	
 	public static WebDriver driver; 
+	
+	//  je change le webdriver
+	public static WebDriver driver2;
+	
 	public static Actions actions;
 	
 	public static void confChrome() throws Exception {
